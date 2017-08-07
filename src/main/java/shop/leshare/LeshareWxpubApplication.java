dc332dc72ac723782910bc741148e31241cbc9b0
leeis <lewis.lynn1006@gmail.com>
2017-08-07 23:06:47 +0800

    first commit

---
+++ src/main/java/shop/leshare/LeshareWxpubApplication.java
@@ -0,0 +1,12 @@
+package shop.leshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeshareWxpubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeshareWxpubApplication.class, args);
+	}
+}
---
+++ src/main/java/shop/leshare/weixin/mp/handler/LogHandler.java
@@ -0,0 +1,30 @@
+package shop.leshare.weixin.mp.handler;
+
+import me.chanjar.weixin.common.session.WxSessionManager;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+import shop.leshare.weixin.mp.utils.JsonUtils;
+
+import java.util.Map;
+
+/**
+ * @author Binary Wang(https://github.com/binarywang)
+ */
+@Component
+public class LogHandler extends AbstractHandler {
+	
+	private Logger logger = LogManager.getLogger(LogHandler.class);
+	
+    @Override
+    public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage,
+                                    Map<String, Object> context, WxMpService wxMpService,
+                                    WxSessionManager sessionManager) {
+        this.logger.info("\n接收到请求消息，内容：{}", JsonUtils.toJson(wxMessage));
+        return null;
+    }
+
+}
